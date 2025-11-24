92eccc477eea96a9a0b806116ed2d4c5a7bc9157
luisashhs <luisgantengpisan@gmail.com>
2025-11-24 10:46:34 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Explore Jabar</title>
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body>
+    <h1 class="text-3xl justify-md underline">hello word</h1>
+</body>
+</html>
